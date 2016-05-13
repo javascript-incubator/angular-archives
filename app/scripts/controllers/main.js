48962e3eccee6b793d7610a049e1934e8588998b
rajatsharma305 <lunasunkaiser@gmail.com>
2016-05-13 20:23:45 +0530

    location added

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,10 @@
  * Controller of the ngRdpApp
  */
 angular.module('ngRdpApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope,$location) {
+    $scope.goToMap = function(){
+      $location.path('/map');
+    }
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
